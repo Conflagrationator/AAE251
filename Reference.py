90b2b9ee97961b244720322548fa9c8d36c847ba
jyeggy <yeggy@purdue.edu>
2017-04-23 22:52:50 -0400

    Updated Reference File
    Units added

--- Reference.py
+++ Reference.py
@@ -2,6 +2,6 @@ dragdV = dragLossUpToAltitude(160)
 gravdV= gravityLossUpToAltitude(160)
 steeringdV= steeringLoss()
 
-dVfor500 = .19540362680297374 * 10**3 + dragdV + gravdV + steeringdV
-dVforGeoTransfer = 2.467596447585046 * 10**3 + dragdV + gravdV + steeringdV
-dVforGeoStatOrbit = 4.4780452652 * 10**3 + dragdV + gravdV + steeringdV
+dVfor500 = .19540362680297374 * 10**3 + dragdV + gravdV + steeringdV # m/s
+dVforGeoTransfer = 2.467596447585046 * 10**3 + dragdV + gravdV + steeringdV # m/s
+dVforGeoStatOrbit = 4.4780452652 * 10**3 + dragdV + gravdV + steeringdV # m/s
