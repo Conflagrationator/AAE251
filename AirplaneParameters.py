98bba71815412bb0a9b664686aef2acf8947b67c
Conflagrationator <willmarrujo@gmail.com>
2017-04-24 21:41:22 -0400

    Removed Test file & Airplane Parameters
    - Each person should have their own instance now.
    - Airplane parameters file

---
+++ AirplaneParameters.py
@@ -0,0 +1,17 @@
+from scipy import *
+
+################################################################################
+# AIRPLANE PARAMETER FUNCTIONS
+################################################################################
+# tsfc = thrust specific fuel consumption
+# CL = airplane lift coefficient
+# CD = airplane drag coefficient
+
+def stallSpeed(weight, freestreamDensity, wingReferenceArea, maxCL):
+    return sqrt((2 * weight) / (freestreamDensity * wingReferenceArea * maxCL))
+
+def turbojetRange(freestreamDensity, wingReferenceArea, CL, CD, tsfc, fullWeight, emptyWeight):
+    return 2 * sqrt(2 / (freestreamDensity * wingReferenceArea)) * (sqrt(CL) / CD) * (1 / tsfc) * (sqrt(fullWeight) - sqrt(emptyWeight))
+
+def turbojetEndurance(tsfc, CL, CD, fullWeight, emptyWeight):
+    return (1 / tsfc) * (CL / CD) * log(fullWeight / emptyWeight)
