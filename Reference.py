7e66983338493efe29b4173a2613376739b5ce3f
Christopher Nilsen <cdn97rockets@gmail.com>
2017-04-24 22:02:13 -0400

    Updated Rocket Values

--- Reference.py
+++ Reference.py
@@ -28,3 +28,12 @@ dVforGeoStatOrbit = 4478.0452652 + dVdragLoss + dVgravityLoss + dVSteeringLoss #
 # ROCKET INFORMATION
 
 # [(name of rocket, Isp, mass, fuel capacity)]
+
+#[Stage 1 , Stage 2]
+f_inert = [.08,.12] # inert mass fraction 
+isp = [310, 360] # Isp 
+dV = [5000,2000] #m/s
+m_pay = 10000 #kg
+dV_needed = 9000 # m/s
+
+
--- rocketEqs.py
+++ rocketEqs.py
@@ -9,47 +9,48 @@ Created on Sun Apr 23 22:06:15 2017
 from scipy import *
 import sys
 
- 
-g = 9.8
-f_inert = .11
-isp = 350
-dV = 1500
-m_pay = 1000
+# 
+#g = 9.8
+#f_inert = .11
+#isp = 350
+#dV = 1500
+#m_pay = 1000
 
 def mass_inert(m_pay, dV, isp, f_inert): # Calculates Intital mass
-    eV = exp(dV/(9.8*isp))
+    eV = exp(dV/(g0*isp))
     
     m_inert = (m_pay*f_inert*(eV-1))/(1-f_inert*eV)
-    print(m_inert)
+    #print(m_inert)
     return(m_inert)
     
     
-def mass_prop(m_pay, dV, g, isp):
-    eV = exp(dV/(9.8*isp))
+def mass_prop(m_pay, dV, g0, isp):
+    eV = exp(dV/(g0*isp))
     
     mProp = (m_pay*(eV-1)*(1-f_inert))/(1-f_inert*eV)
-    print(mProp)
+    
+    #print(mProp)
     return(mProp)
     
-def initial_mass(m_pay,f_inert, dV, g, isp):
+def initial_mass(m_pay,f_inert, dV, g0, isp):
     
-    eV = exp(dV/(9.8*isp))
+    eV = exp(dV/(g0*isp))
     
     initialMass = ((m_pay*eV)*(1-f_inert))/(1-f_inert*eV)
-    print(initialMass)
+    #print(initialMass)
     return(initialMass)
     
-def deltaV(f_inert,g,isp): 
-    deltaV = -(log(f_inert))*g*isp
-    print(deltaV)
+def deltaV(f_inert,g0,isp): 
+    deltaV = -(log(f_inert))*g0*isp
+    #print(deltaV)
     return(deltaV)
     
 
 
     
-mass_inert(m_pay, dV, isp, f_inert)
-mass_prop(m_pay, dV, g, isp)
-initial_mass(m_pay,f_inert, dV, g, isp)   
-deltaV(f_inert,g,isp)
-
+#mass_inert(m_pay, dV, isp, f_inert)
+#mass_prop(m_pay, dV, g, isp)
+#initial_mass(m_pay,f_inert, dV, g, isp)   
+#deltaV(f_inert,g,isp)
+#
 
