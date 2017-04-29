dcad7c325c90771e057756838fadd8ad82c4bb82
jyeggy <yeggy@purdue.lcl>
2017-04-28 22:11:45 -0400

    Format

--- airplaneCalculations.py
+++ airplaneCalculations.py
@@ -49,6 +49,7 @@ landingDist = landingDistance(0, takeoffWeight, VStall0, Thrust, wingspan, chord
 ################################################################################
 # PRICING
 ################################################################################
+
 Cost = costOfFuel(fuelWeight) + costOfInertMass(emptyWeight)
 
 ################################################################################
